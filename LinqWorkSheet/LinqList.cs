48bffa4981be764c1b4f94b9b9b19adfc56d36d3
unknown <adamjsteinbacher@gmail.com>
2020-07-07 14:50:45 -0500

    question 1 done

--- LinqWorkSheet/LinqList.cs
+++ LinqWorkSheet/LinqList.cs
@@ -8,5 +8,17 @@ namespace LinqWorkSheet
 {
     class LinqList
     {
+        static void Main1()
+        {
+            var list = new List<string>() { "the", "bike", "this", "it", "thenth", "mathamatics" };
+            // Use extension method.
+            var names = list.Where (l => l.Contains("th"));
+            // Use instance method.
+            foreach (var item in names )
+            {
+                Console.WriteLine();
+            }
+            
+        }
     }
 }
