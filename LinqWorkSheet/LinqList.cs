ee8bb5927fc04929e56045910bbcb4b0622eb8a7
unknown <adamjsteinbacher@gmail.com>
2020-07-08 21:12:23 -0500

    question 3 done

--- LinqWorkSheet/LinqList.cs
+++ LinqWorkSheet/LinqList.cs
@@ -32,18 +32,60 @@ namespace LinqWorkSheet
             }
            
         }
-
-
         static void Main3() 
         {
-            List<string> namesClassGrades = new List<string>() 
+            List<string> studentOne = new List<string>() // list one
             { 
                 "80, 100, 92, 89, 65",
-                "93, 81, 78, 84, 69",
-                "73, 88, 83, 99, 69",
-                "98, 100, 66, 74, 55"
             };
+           
+            var skipLowestGrade = studentOne.Skip(4);
+            foreach (var value in skipLowestGrade)
+            {
+                Console.WriteLine("SKIP 4: {4}", value);// drop 65
+            }
+            
+            List<string> studentTwo = new List<string>() //list2
+            {
+                "93, 81, 78, 84, 69",
+            };
+            
+            foreach (var value in skipLowestGrade)
+            {
+                Console.WriteLine("SKIP 4: {4}", value);// drop 69
+            }
 
+            List<string> studentThree = new List<string>() //list 3
+            {
+                 "73, 88, 83, 99, 69",
+            };
+            foreach (var value in skipLowestGrade)
+            {
+                Console.WriteLine("SKIP 4: {4}", value); // drop 69
+            }
+
+            List<string> studentFour = new List<string>() //list 4
+            {
+                "98, 100, 66, 74, 55"
+            }; 
+            foreach (var value in skipLowestGrade)
+            {
+                Console.WriteLine("SKIP 4: {4}", value); // drop 55
+            }
+
+            
+            var grades = new[] { studentOne, studentTwo, studentThree, studentFour } // all best grades to one list
+                     .SelectMany(id => id)
+                     .ToList();
+           
+            // Use Average to compute average value.
+            double[] array = {80, 100, 92, 89, 93, 81, 78, 84, 73, 88, 83, 99, 98, 100, 66, 74};
+            double average = array.Average();
+            Console.WriteLine("AVERAGE: {0}", average);
+
+
+
+            
 
 
 
